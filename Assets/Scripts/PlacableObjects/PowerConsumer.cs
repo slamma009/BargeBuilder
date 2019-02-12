5667219bb9af390066956011f7ee479556ca885b
slamma009 <alsnurb@gmail.com>
2019-02-11 21:18:19 -0600

    Light Update
    Power consumer was changed to utalize the tick system. Created a SimpleLight that inherits from it that will utalize power from the grid and turn on a light.

--- Assets/Scripts/PlacableObjects/PowerConsumer.cs
+++ Assets/Scripts/PlacableObjects/PowerConsumer.cs
@@ -4,28 +4,56 @@ using UnityEngine;
 
 public class PowerConsumer : ElectricalPole
 {
-    public float TickLength = 1;
-    public int WattConsumtion = 1;
+    public float WattConsumtion = 0.2f;
 
-    private float SavedTime; // Saved time for burning objects
-    
-    private void Update()
+    private bool _HasPower;
+
+    public override void ObjectPlaced()
+    {
+        TickController.TickEvent += TickUpdate;
+        base.ObjectPlaced();
+    }
+
+    private void TickUpdate(object sender, TickArgs args)
     {
         if (!Placed)
             return;
-
-        if (Time.timeSinceLevelLoad - SavedTime >= TickLength)
+        
+        if (Power.NodeGroups[GroupId].UsePower(WattConsumtion))
         {
-            if (Power.NodeGroups[GroupId].UsePower(WattConsumtion))
+            if (!_HasPower)
             {
-                SavedTime = Time.timeSinceLevelLoad;
-                Execute();
+                _HasPower = true;
+                PowerOn();
+            }
+            PowerUsed();
+        } else
+        {
+            if (_HasPower)
+            {
+                _HasPower = false;
+                PowerOff();
             }
         }
     }
 
-    protected virtual void Execute()
+    protected virtual void PowerOn()
     {
 
+    }
+
+    protected virtual void PowerOff()
+    {
+
+    }
+    protected virtual void PowerUsed()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        if(Placed)
+            TickController.TickEvent -= TickUpdate;
     }
 }
---
+++ Assets/Scripts/PlacableObjects/SimpleLight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class SimpleLight : PowerConsumer
+{
+    public Light Lamp = new Light();
+    protected override void PowerOn()
+    {
+        Lamp.enabled = true;
+        base.PowerOn();
+    }
+
+    protected override void PowerOff()
+    {
+        Lamp.enabled = false;
+        base.PowerOn();
+    }
+}
+
