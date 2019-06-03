fa8c14ae5b366a770dcf7414136c21c615089edf
slamma009 <alsnurb@gmail.com>
2019-06-03 18:54:25 -0500

    Update Extractor to use TickUpdate
    Changed the extractor to no longer use the FixedUpdate but rather the TickUpdate

--- Assets/Scripts/PlacableObjects/Extractor.cs
+++ Assets/Scripts/PlacableObjects/Extractor.cs
@@ -4,49 +4,71 @@ using UnityEngine;
 
 public class Extractor : PlacableObject
 {
+    /// <summary>
+    /// The position to spawn the ore at
+    /// </summary>
     public Transform OreSpawn;
-    public float SpawnRatePerMinute = 20;
+
+    /// <summary>
+    /// Base spawn time in ticks
+    /// </summary>
+    public float SpawnRate = 15;
+
+    /// <summary>
+    /// The item to spawn
+    /// </summary>
     public Item SpawnObject;
+    
     public Transform OutputAnchor;
 
-    private float TimeSinceLastSpawn;
-    private float SecondsToSpawn;
+    private float _ticksSinceLastSpawn;
 
-    private ConveyorBelt OutputObject;
+    private ConveyorBelt outputObject;
 
     
     public void AnchorChanged(AnchorObject obj)
     {
         if (obj.ConnectAnchor == null)
-            OutputObject = null;
+            outputObject = null;
         else
         {
             ConveyorBelt belt = obj.ConnectAnchor.transform.GetTopParent().GetComponent<ConveyorBelt>();
             if(belt != null)
-                OutputObject = belt;
+                outputObject = belt;
             else
-                OutputObject = null;
+                outputObject = null;
         }
     }
 
     public override void ObjectPlaced()
     {
         Anchors[0].AnchorChanged = AnchorChanged;
+
+        // Add the TickUpdate to the Tick Event
+        TickController.TickEvent += TickUpdate;
         base.ObjectPlaced();
     }
 
-    public void FixedUpdate()
+
+    public void TickUpdate(object sender, TickArgs arg)
     {
-        if (Placed && OutputObject != null && OutputObject.CanTakeItem(SpawnObject))
+        if (Placed && outputObject != null && outputObject.CanTakeItem(SpawnObject))
         {
-            TimeSinceLastSpawn += Time.deltaTime;
-            if (TimeSinceLastSpawn > 60 / SpawnRatePerMinute)
+            _ticksSinceLastSpawn++;
+            if (_ticksSinceLastSpawn > SpawnRate)
             {
-                TimeSinceLastSpawn = 0;
+                _ticksSinceLastSpawn = 0;
                 Item newObj = Instantiate(SpawnObject, OreSpawn.position, Quaternion.identity).GetComponent<Item>();
-                OutputObject.PushItem(newObj);
+                outputObject.PushItem(newObj);
             }
         }
+    }
+
+    // Remove TickUpdate from the event list if it has been set.
+    private void OnDestroy()
+    {
+        if (Placed)
+            TickController.TickEvent -= TickUpdate;
     }
 
 }
